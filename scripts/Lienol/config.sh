b6307625abf74db2b8ffeb69b8ef1a7f4d9f99f0
Larify <luowehua7@gmail.com>
2021-12-14 20:16:40 +0800

    chore: test

--- scripts/Lienol/config.sh
+++ scripts/Lienol/config.sh
@@ -4,7 +4,7 @@
 
 ### 系统配置 ###
 # 修改路由器IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.5/g' package/base-files/files/bin/config_generate
 
 DEFAULT_SETTINGS_FILE="package/lean/default-settings/files/zzz-default-settings"
 # 修改默认密码
@@ -12,15 +12,15 @@ DEFAULT_SETTINGS_FILE="package/lean/default-settings/files/zzz-default-settings"
 # 修改主机名
 sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt'" $DEFAULT_SETTINGS_FILE
 # 修改默认主题
-sed -i "/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/bootstrap" $DEFAULT_SETTINGS_FILE
+# sed -i "/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/bootstrap" $DEFAULT_SETTINGS_FILE
 
-# 修改一下固件版本，方便查看固件编译时间
-current_build_date=`date +"%Y/%m/%d"`
-sed -i "s|<%=pcdata(ver.distversion)%>|<%=pcdata(ver.distversion)%>(${current_build_date})|g" package/lean/autocore/files/arm/index.htm
-sed -i "s|<%=pcdata(ver.distversion)%>|<%=pcdata(ver.distversion)%>(${current_build_date})|g" package/lean/autocore/files/x86/index.htm
+# # 修改一下固件版本，方便查看固件编译时间
+# current_build_date=`date +"%Y/%m/%d"`
+# sed -i "s|<%=pcdata(ver.distversion)%>|<%=pcdata(ver.distversion)%>(${current_build_date})|g" package/lean/autocore/files/arm/index.htm
+# sed -i "s|<%=pcdata(ver.distversion)%>|<%=pcdata(ver.distversion)%>(${current_build_date})|g" package/lean/autocore/files/x86/index.htm
 
-### 插件配置 ###
-# wrtbwmon默认刷新时间更改为 3 秒
-sed -i 's/interval: 5/interval: 3/g' package/lean/luci-app-wrtbwmon/htdocs/luci-static/wrtbwmon/wrtbwmon.js
-# 修改流量统计写入为10分钟
-sed -i 's/option commit_interval 4h/option commit_interval 10m/g' feeds/packages/net/nlbwmon/files/nlbwmon.config
+# ### 插件配置 ###
+# # wrtbwmon默认刷新时间更改为 3 秒
+# sed -i 's/interval: 5/interval: 3/g' package/lean/luci-app-wrtbwmon/htdocs/luci-static/wrtbwmon/wrtbwmon.js
+# # 修改流量统计写入为10分钟
+# sed -i 's/option commit_interval 4h/option commit_interval 10m/g' feeds/packages/net/nlbwmon/files/nlbwmon.config
