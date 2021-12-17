337273408afb6369f3e51240315dd9d04f425769
Larify <luowehua7@gmail.com>
2021-12-17 16:00:33 +0800

    chore: app

--- scripts/Lienol/config.sh
+++ scripts/Lienol/config.sh
@@ -7,6 +7,8 @@ DEFAULT_SETTINGS_FILE="package/default-settings/files/zzz-default-settings"
 # 版本号增加构建日期（请保留Lienol相关的版本信息）
 FW_BUILD_DATE=`date +"%Y/%m/%d"`
 sed -i "s|OpenWrt SNAPSHOT|OpenWrt (${FW_BUILD_DATE})|g" $DEFAULT_SETTINGS_FILE
+# 禁用DHCP服务（旁路由模式）
+sed -i 's|exit|uci set dhcp.lan.ignore="1"\nuci set dhcp.lan.dynamicdhcp="0"\nuci commit dhcp|g' $DEFAULT_SETTINGS_FILE
 
 # 修改NTP时间同步服务器
 CONFIG_GENERATE_FILE="package/base-files/files/bin/config_generate"
@@ -16,6 +18,6 @@ sed -i "s|2.openwrt.pool.ntp.org|time.ustc.edu.cn|g" $CONFIG_GENERATE_FILE
 sed -i "s|3.openwrt.pool.ntp.org|cn.pool.ntp.org|g" $CONFIG_GENERATE_FILE
 
 ### 删除一些不需要的包，可能造成问题 ###
+rm -rf package/feeds/openwrt-luci/themes/luci-theme-bootstrap-mod
 
 ### 插件配置 ###
-
